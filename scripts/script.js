e1cfd8ad82d0d41aca7a2dd90c58388ad082fac1
David <pametnik@abv.bg>
2021-02-02 10:49:47 +0200

    update
    add button.css
    add kittens.css
    add main.css
    small changes in JS file

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,20 @@ const loadKittens = document.querySelector('.load-kittens');
 loadKittens.addEventListener("click", fetchKittens);
 let pop = document.querySelector(".popUp");
 let kittenInfo = document.querySelector(".kitten-info");
+let kitenPopUp = document.querySelector(".bg-modal");
+let close = document.querySelector('.close');
+let bgModal = document.querySelector('.bg-modal');
 
-let bigCat;
+function moreInfoForCat(event) {
 
- function moreInfo(event) {
-    getKittenDetailsById(event.target.id).then((oneCat) => {
-        let moreInfo = `<a>
-    	<figure class="thumbnail">
-    		<img id="${oneCat.id}" src=${oneCat.img} alt="meow">
+	let cursorStyle = document.getElementById(event.target.id);
+	cursorStyle.style.cursor = 'wait';
+
+	getKittenDetailsById(event.target.id).then((oneCat) => {
+
+		let moreInfo = `<a>
+		<figure class="thumbnail">
+    	<img id="${oneCat.id}" src=${oneCat.img} alt="meow">
     	</figure>
     	<div class="card-content">
     		<h2>${oneCat.name}</h2>
@@ -21,22 +27,30 @@ let bigCat;
     		<p>Cuteness:${oneCat.additional_details.cuteness_level}</p>
     	</div>
     	<!-- .card-content -->
-    </a>`
-    	kittenInfo.style.display = 'block';
-    	kittenInfo.innerHTML = moreInfo;
-    });
+	</a>`
+		kittenInfo.innerHTML = moreInfo;
+		kitenPopUp.style.display = 'flex';
+		cursorStyle.style.cursor = 'pointer';
+	});
 }
 
 function hanleButtonState(disabled) {
-    loadKittens.disabled = disabled;
-    let buttonText = disabled ? 'Loading <span></span>' : 'Load kittens'
-    loadKittens.innerHTML = buttonText;
+	loadKittens.disabled = disabled;
+	let buttonText = disabled ? 'Loading <span></span>' : 'Load kittens'
+	loadKittens.innerHTML = buttonText;
 }
 
-kittenInfo.addEventListener('click', () => {
-	kittenInfo.style.display = 'none';
-	hanleButtonState(false);
+bgModal.addEventListener('click', (event) => {
+	if (event.target !== bgModal) {
+		return
+	}
+	kitenPopUp.style.display = 'none';
+})
+
+close.addEventListener('click', () => {
+	kitenPopUp.style.display = 'none';
 });
+
 
 pop.addEventListener('click', () => {
 	pop.style.display = 'none';
@@ -44,25 +58,28 @@ pop.addEventListener('click', () => {
 });
 
 function fetchKittens() {
-    hanleButtonState(true);
+	hanleButtonState(true);
 
 	getKittens().then((cats) => {
-		bigCat = cats;
+
 		let sait = " ";
 		for (let i = 0; i < cats.length; i++) {
-			sait += renderHTML(cats[i]);
+			sait += populateHTMLwithCats(cats[i]);
 		}
 		document.querySelector(".cards").innerHTML = sait;
 		let card = document.querySelectorAll(".card");
-		card.forEach((el)=>{ el.addEventListener("click", moreInfo);});
-	}).catch((error)=>{
+		card.forEach((el) => {
+			el.style.cursor = "pointer"
+			el.addEventListener("click", moreInfoForCat);
+		});
+	}).catch((error) => {
 		console.log(error);
 		pop.style.display = 'flex';
 		pop.innerHTML = error;
 	})
 }
 
-function renderHTML(element) {
+function populateHTMLwithCats(element) {
 
 	return `<article id="${element.id}" class="card">
 	<a>
